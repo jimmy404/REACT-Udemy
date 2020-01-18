73511d49624d3f767fb18123daa4c55e507ff481
Jimmy <bruno_pier@hotmail.com>
2020-01-18 18:20:20 -0300

    Adding imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react'
-import EjemploDeComponentDidMount from './sections/life-cycle/componentDidMount';
+import React, { Component } from 'react';
+import FetchExample from './sections/fetch-example';
 
-
-class App extends Component {
+class FetchExample extends Component {
   render () {
     return (
       <div className="App">
-      <EjemploDeComponentDidMount/>
+        <h4>Fetch example</h4>
       </div>
     );
   }
 }
 
-export default App;
+export default FetchExample;
